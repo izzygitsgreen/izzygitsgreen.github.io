676f1374fe2782fb9f688a73515725ae57b63754
Isabella J. Tighe <tighephoon@gmail.com>
2020-09-05 20:15:18 -0700

    fixing push

--- about.js
+++ about.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function openModal() {
   document.getElementById("myModal").style.display = "block";
 }
 
 function closeModal() {
   document.getElementById("myModal").style.display = "none";
-=======
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
->>>>>>> 32aa876c3e97ed28bb740993f282ec00fe1dc80e
 }
