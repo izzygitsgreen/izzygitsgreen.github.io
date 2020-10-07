ded395fc0c6317e2d90a013675bb040a29a64692
Isabella J. Tighe <tighephoon@gmail.com>
2020-10-07 09:40:09 -0400

    portfolio project html

---
+++ project.js
@@ -0,0 +1,10 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
