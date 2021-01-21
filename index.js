7e6afd247ca238ad3f20568ca9005bcee1f472ce
Isabella J. Tighe <tighephoon@gmail.com>
2021-01-20 21:26:42 -0800

    added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
